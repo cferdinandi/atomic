3ff37c6088bd8eb969fcf1a4683af86aac5748cf
cferdinandi <chris@gomakethings.com>
2017-03-22 14:24:36 -0400

    v1.2.0 - Add support for JSONP - Fixed UMD header to properly pass in root (addresses https://github.com/toddmotto/atomic/issues/25)

--- dist/atomic.js
+++ dist/atomic.js
@@ -1,95 +1,114 @@
-/*! atomic v1.0.0 | (c) 2015 @toddmotto | https://github.com/toddmotto/atomic */
+/*! atomic v1.2.0 | (c) 2017 @toddmotto | https://github.com/toddmotto/atomic */
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory;
-  } else {
-    root.atomic = factory(root);
-  }
-})(this, function (root) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory(root));
+	} else if (typeof exports === 'object') {
+		module.exports = factory(root);
+	} else {
+		root.atomic = factory(root);
+	}
+})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
 
-  'use strict';
+	'use strict';
 
-  var exports = {};
+	var exports = {};
 
-  var config = {
-    contentType: 'application/x-www-form-urlencoded'
-  };
+	var config = {
+		contentType: 'application/x-www-form-urlencoded'
+	};
 
-  var parse = function (req) {
-    var result;
-    try {
-      result = JSON.parse(req.responseText);
-    } catch (e) {
-      result = req.responseText;
-    }
-    return [result, req];
-  };
+	var parse = function (req) {
+		var result;
+		try {
+			result = JSON.parse(req.responseText);
+		} catch (e) {
+			result = req.responseText;
+		}
+		return [result, req];
+	};
 
-  var xhr = function (type, url, data) {
-    var methods = {
-      success: function () {},
-      error: function () {},
-      always: function () {}
-    };
-    var XHR = root.XMLHttpRequest || ActiveXObject;
-    var request = new XHR('MSXML2.XMLHTTP.3.0');
+	var xhr = function (type, url, data) {
+		var methods = {
+			success: function () {},
+			error: function () {},
+			always: function () {}
+		};
+		var XHR = root.XMLHttpRequest || ActiveXObject;
+		var request = new XHR('MSXML2.XMLHTTP.3.0');
 
-    request.open(type, url, true);
-    request.setRequestHeader('Content-type', config.contentType);
-    request.onreadystatechange = function () {
-      var req;
-      if (request.readyState === 4) {
-        req = parse(request);
-        if (request.status >= 200 && request.status < 300) {
-          methods.success.apply(methods, req);
-        } else {
-          methods.error.apply(methods, req);
-        }
-        methods.always.apply(methods, req);
-      }
-    };
-    request.send(data);
+		request.open(type, url, true);
+		request.setRequestHeader('Content-type', config.contentType);
+		request.onreadystatechange = function () {
+			var req;
+			if (request.readyState === 4) {
+				req = parse(request);
+				if (request.status >= 200 && request.status < 300) {
+					methods.success.apply(methods, req);
+				} else {
+					methods.error.apply(methods, req);
+				}
+				methods.always.apply(methods, req);
+			}
+		};
+		request.send(data);
 
-    var atomXHR = {
-      success: function (callback) {
-        methods.success = callback;
-        return atomXHR;
-      },
-      error: function (callback) {
-        methods.error = callback;
-        return atomXHR;
-      },
-      always: function (callback) {
-        methods.always = callback;
-        return atomXHR;
-      }
-    };
+		var atomXHR = {
+			success: function (callback) {
+				methods.success = callback;
+				return atomXHR;
+			},
+			error: function (callback) {
+				methods.error = callback;
+				return atomXHR;
+			},
+			always: function (callback) {
+				methods.always = callback;
+				return atomXHR;
+			}
+		};
 
-    return atomXHR;
-  };
+		return atomXHR;
+	};
 
-  exports.get = function (src) {
-    return xhr('GET', src);
-  };
+	var jsonp = function (url, callback) {
+		// Create script with url and callback
+		var ref = root.document.getElementsByTagName( 'script' )[ 0 ];
+		var script = root.document.createElement( 'script' );
+		script.src = url + (url.indexOf( '?' ) + 1 ? '&' : '?') + 'callback=' + callback;
 
-  exports.put = function (url, data) {
-    return xhr('PUT', url, data);
-  };
+		// Insert script tag into the DOM (append to <head>)
+		ref.parentNode.insertBefore( script, ref );
 
-  exports.post= function (url, data) {
-    return xhr('POST', url, data);
-  };
+		// After the script is loaded (and executed), remove it
+		script.onload = function () {
+			this.remove();
+		};
+	};
 
-  exports.delete = function (url) {
-    return xhr('DELETE', url);
-  };
+	exports.get = function (src) {
+		return xhr('GET', src);
+	};
 
-  exports.setContentType = function(value) {
-    config.contentType = value;
-  };
+	exports.put = function (url, data) {
+		return xhr('PUT', url, data);
+	};
 
-  return exports;
+	exports.post= function (url, data) {
+		return xhr('POST', url, data);
+	};
+
+	exports.delete = function (url) {
+		return xhr('DELETE', url);
+	};
+
+	exports.jsonp = function (url, callback) {
+		return jsonp(url, callback);
+	};
+
+	exports.setContentType = function(value) {
+		config.contentType = value;
+	};
+
+	return exports;
 
 });
