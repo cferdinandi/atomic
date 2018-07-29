9205e7ed88ed7b597390ee8f2527038707341dac
Gary Green <holegary@gmail.com>
2018-07-29 01:53:23 +0100

    Fix and update unit tests, npm test to run.

--- test/spec/atomic-spec.js
+++ test/spec/atomic-spec.js
@@ -15,57 +15,26 @@ describe('atomic', function () {
 		});
 
 		it('should open an XMLHttpRequest', function () {
-			atomic.ajax({
-				url: '/endpoint'
-			})
-				.success(function (data, xhr) {})
-				.error(function (data, xhr) {})
-				.always(function(data, xhr) {});
+			atomic('/endpoint')
+				.then(function (response) {})
+				.catch(function (error) {});
 			expect(XMLHttpRequest.prototype.open).toHaveBeenCalled();
 		});
 
 		it('should send and XMLHttpRequest', function () {
-			atomic.ajax({
-				url: '/endpoint'
-			})
-				.success(function (data, xhr) {})
-				.error(function (data, xhr) {});
+			atomic('/endpoint')
+			.then(function (response) {})
+			.catch(function (error) {});
 			expect(XMLHttpRequest.prototype.send).toHaveBeenCalled();
 		});
 
 		it('should set request header', function(){
-			atomic.ajax({
-				url: '/endpoint'
-			})
-				.success(function (data, xhr) {})
-				.error(function (data, xhr) {})
-				.always(function(data, xhr){});
+			atomic('/endpoint')
+			.then(function (response) {})
+			.catch(function (error) {});
 			expect(XMLHttpRequest.prototype.setRequestHeader).toHaveBeenCalled();
 		});
 
-	});
-
-	describe('always', function() {
-
-		it('should be called last after success or error', function(done) {
-			var result = 0;
-			atomic.ajax({
-				url: '/endpoint'
-			})
-				.always(function(data, xhr) {
-					result = 3;
-				})
-				.success(function(data, xhr) {
-					result  = 1;
-				})
-				.error(function(data, xhr) {
-					result  = 2;
-				});
-			setTimeout(function() {
-				expect(result).toEqual(3);
-				done();
-			}, 100);
-		});
 	});
 
 	describe('contentType', function(){
@@ -75,17 +44,14 @@ describe('atomic', function () {
 		});
 
 		it('should use "application/x-www-form-urlencoded" as default Content-type', function(){
-			atomic.ajax({
-				url: '/endpoint'
-			});
+			atomic('/endpoint');
 
 			expect(XMLHttpRequest.prototype.setRequestHeader)
 					.toHaveBeenCalledWith('Content-type', 'application/x-www-form-urlencoded');
 		});
 
 		it('should set Content-type', function() {
-			atomic.ajax({
-				url: '/endpoint',
+			atomic('/endpoint', {
 				headers: {
 					'Content-type': 'application/json'
 				}
